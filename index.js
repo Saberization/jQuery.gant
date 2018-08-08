91e077b39d8a44c09723ddbacde48251a3adef44
KeepPassionAlways <406142765@qq.com>
2018-08-08 10:03:38 +0800

    update

---
+++ index.js
@@ -0,0 +1,81 @@
+/*
+ * @Author: guotq
+ * @Date: 2018-08-03 16:07:12
+ * @Last Modified by: guotq
+ * @Last Modified time: 2018-08-08 10:02:29
+ * @Description: 甘特图数据
+ */
+
+(function (win, $) {
+    "use strict";
+
+    var getDateTime = function (dateTime) {
+
+        if (typeof dateTime === 'number') {
+            return "/Date(" + dateTime + ")/";
+        } else if (typeof dateTime === 'string') {
+            return "/Date(" + new Date(dateTime).getTime() + ")/";
+        }
+    };
+
+    var renderGant = function (data) {
+        $(".gantt").gantt({
+            source: data,
+            navigate: "scroll",
+            scale: "weeks",
+            minScale: "days",
+            maxScale: "months",
+            waitText: '加载中...',
+            months: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+        });
+    };
+
+    var getGant = function () {
+        var result = [];
+
+        $.ajax({
+            url: './data.json',
+            type: 'post',
+            success: function (res) {
+                var list = res.list;
+
+                for (var i = 0, len = list.length; i < len; i++) {
+                    var item = list[i],
+                        overdueDay = item.overdueDay,
+                        endTime = item.endTime,
+                        overduePer = '';
+
+                    // 如果超期时间，endTime要延长
+                    if (overdueDay) {
+                        endTime = getOverduemillisecond(endTime, overdueDay);
+                        overduePer = ((endTime - item.endTime) / item.endTime * 100).toFixed(2);
+                        overduePer = overduePer * 100 + '%';
+                    }
+
+                    result.push({
+                        name: item.name,
+                        values: [{
+                            from: getDateTime(item.startTime),
+                            to: getDateTime(endTime),
+                            label: item.finishPer,
+                            customClass: 'default',
+                            overduePer: overduePer,
+                            finishPer: item.finishPer
+                        }]
+                    });
+                }
+
+                console.log(result);
+                // 渲染Gant
+                renderGant(result);
+            }
+        });
+    };
+
+    var getOverduemillisecond = function (endTime, overdueDay) {
+        return endTime + (overdueDay * 24 * 60 * 60 * 1000);
+    };
+
+    // 获取 Gant 数据
+    getGant();
+}(this, jQuery));
